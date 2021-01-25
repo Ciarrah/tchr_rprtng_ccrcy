a7ae35d13d2c5df529f3cbe7f2b0089180ac02ed
Ciarrah <50405084+Ciarrah@users.noreply.github.com>
2021-01-25 12:18:31 +0000

    Additional heritability plot created

--- Forest_plots.R
+++ Forest_plots.R
@@ -75,3 +75,40 @@ forestplot(tabletext,
            line.margin = 0.25,
            xlab = "Standardised effect estimate (95% CI)")
 dev.off()
+
+## Heritability
+
+heritability=read_excel("/Volumes/MRC-IEU-research/projects/ieu2/p1/015/working/data/CB_2021/heritability_forest_plot_input.xlsx")
+heritability=as.data.frame(heritability)
+heritability[,c("Mean","SE","Lower","Upper")]=round(heritability[,c("Mean","SE","Lower","Upper")],3)
+lbl=c("KS2","KS3")
+heritability.adj=round(heritability$Mean[seq(0,4,2)],3) #adjusted
+a=paste0(heritability.adj)
+heritability.unadj=round(heritability$Mean[seq(1,4,2)],3) #unadjusted
+b=paste0(heritability.unadj)
+heritability=transform(heritability, "95% CI"=paste0("(",paste(Lower, Upper, sep=", "),paste0(")")))
+e=paste0(heritability$X95..CI)
+e.U=e[seq(1,4,2)]
+e.A=e[seq(0,4,2)]
+tabletext=cbind(c("Key stage",lbl),c("Unadjusted est.",b),c("95% CI",e.U),c("Adjusted est.",a),c("95% CI",e.A))
+
+png("HERIT_FP_2021.png",width=5000,height=2000,res=300)
+forestplot(tabletext,
+           hrzl_lines = list("1"=gpar(lty=1),"2"=gpar(lty=1)),
+           
+           legend = c("Adjusted","Unadjusted"),fn.ci_norm = c(fpDrawNormalCI, fpDrawCircleCI),
+           title = "Heritability plot",
+           mean = cbind(c(NA,heritability$Mean[heritability$`Analyses` == "Adjusted"]), 
+                        c(NA,heritability$Mean[heritability$`Analyses` == "Unadjusted"])),
+           lower = cbind(c(NA,heritability$Lower[heritability$`Analyses` == "Adjusted"]), 
+                         c(NA,heritability$Lower[heritability$`Analyses` == "Unadjusted"])),
+           upper = cbind(c(NA,heritability$Upper[heritability$`Analyses` == "Adjusted"]), 
+                         c(NA,heritability$Upper[heritability$`Analyses` == "Unadjusted"])),
+           col=fpColors(box=c("#4393C3", "red2"),line=c("#4393C3", "red2"),
+                        zero=c("black")),cex = 1,
+           boxsize = 0.25,
+           line.margin = 0.25,
+           xlab = "Standardised effect estimate (95% CI)")
+dev.off()
+
+
